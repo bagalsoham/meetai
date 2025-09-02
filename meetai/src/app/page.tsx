712faf7b75694b103f33d0e042dfe5865363fc36
Soham <bagalsoham1717@gmail.com>
2025-09-02 21:47:03 +0530

    feat:better auth added

--- meetai/src/app/page.tsx
+++ meetai/src/app/page.tsx
@@ -1,10 +1,35 @@
+"use client"; // ← Add this at the top
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    
+    <form>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />  
       <Button>Get Started</Button>
-    
+    </form>
   );
 }
