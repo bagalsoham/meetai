b3073c0563ebaa57d68a2ac3c1236bdda4b60f2e
Soham <bagalsoham1717@gmail.com>
2025-09-07 19:07:14 +0530

    feat: enables pagination component

--- meetai/src/modules/agents/ui/views/agents-view.tsx
+++ meetai/src/modules/agents/ui/views/agents-view.tsx
@@ -7,9 +7,10 @@ import { columns, Payment } from "../components/columns";
 import { DataTable } from "../components/data-table";
 import EmptyState from "@/components/empty-state";
 import { useAgentsFilter } from "@/app/(dashboard)/agents/hooks/use-agents-flters";
+import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
-  const [filters] =useAgentsFilter();
+  const [filters,setFilters] =useAgentsFilter();
 
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -27,6 +28,11 @@ export const AgentsView = () => {
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
       <DataTable data={data.items} columns={columns} />
+      <DataPagination
+      page={filters.page}
+      totalPages={data.totalPages}
+      onPageChange={(page)=> setFilters({page})}
+      />
       {
         data.items.length ===0 &&(
           <EmptyState
